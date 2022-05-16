59fc057d9cad22a7e592b7c8f89d0e75ca91e8a6
Alexander <a.pride.xxi@tutanota.com>
2022-05-16 13:45:55 +0300

    Initital Commit

---
+++ uMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uMessenger
+//
+//  Created by Александр Прайд on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .link
+    }
+
+
+}
+
